f17dd3c84c31f28c6b8fa87fcfb232ee591b9b2a
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-08-23 17:19:50 -0500

    Time: 416 ms (95.22%), Space: 29.5 MB (5.78%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,11 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-
-        nums_set = set()
-
-        for num in nums:
-            if num in nums_set:
-                return True
-            nums_set.add(num)
-        return False
+        
+        nums_set = set(nums)
+        
+        return len(nums_set) != len(nums)
