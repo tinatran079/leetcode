3889cfb0ffbf836901e6b25483a379cd681b7058
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-01 17:30:22 -0500

    Time: 21 ms (55.67%), Space: 13.3 MB (59.21%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,31 +1,25 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
-        """
+        # Initialize pointers for nums1 and nums2
+        i = m - 1  # Pointer for nums1
+        j = n - 1  # Pointer for nums2
+        last = m + n - 1  # Last index of the merged array nums1
         
-        # get the last index of nums1 - length
-        last = m + n -1
-        
-        # merge in rev order, while there are elements in both arrays
-        while m > 0 and n > 0:
-            if nums1[m - 1] > nums2[n - 1]:
-                # go to last pos and replace
-                nums1[last] = nums1[m - 1]
-                #update ptrs
-                m -= 1
+        # Merge in reverse order while there are elements in both arrays
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[last] = nums1[i]
+                i -= 1
             else:
-                nums1[last] = nums2[n -1]
-                n -= 1
+                nums1[last] = nums2[j]
+                j -= 1
             last -= 1
-        # edge case - if nums2 is not empty - fill nums1 w leftover nums2
-        while n > 0:
-            nums1[last] = nums2[n - 1]
-            n, last = n - 1, last -1
+        
+        # Edge case - if nums2 is not empty, fill nums1 with leftover nums2
+        while j >= 0:
+            nums1[last] = nums2[j]
+            j -= 1
+            last -= 1
         
         
         
