b6e86f23f7ccd81319dd8e24a64b043bf23820a7
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-01 17:33:16 -0500

    Time: 22 ms (47.71%), Space: 13.2 MB (59.21%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,26 +1,21 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        # Initialize pointers for nums1 and nums2
-        i = m - 1  # Pointer for nums1
-        j = n - 1  # Pointer for nums2
-        last = m + n - 1  # Last index of the merged array nums1
+        last = m + n - 1
         
-        # Merge in reverse order while there are elements in both arrays
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
-                nums1[last] = nums1[i]
-                i -= 1
+        while m > 0 and n > 0:
+            if nums1[m-1] < nums2[n-1]:
+                nums1[last] = nums2[n-1]
+                n -= 1
             else:
-                nums1[last] = nums2[j]
-                j -= 1
+                nums1[last] = nums1[m -1]
+                m -=1
             last -= 1
         
-        # Edge case - if nums2 is not empty, fill nums1 with leftover nums2
-        while j >= 0:
-            nums1[last] = nums2[j]
-            j -= 1
+        while n > 0:
+            nums1[last] = nums2[n -1]
             last -= 1
-        
+            n -= 1
+                
         
         
         
