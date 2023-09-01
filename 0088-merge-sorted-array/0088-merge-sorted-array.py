3d1c2893109ae56f3616115087e86a7bbf1a3c6e
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-01 17:10:36 -0500

    Time: 14 ms (93.38%), Space: 13.2 MB (59.21%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -8,22 +8,25 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
         
-        # last index of nums1
-        last = m + n - 1
+        # get the last index of nums1 - length
+        last = m + n -1
         
-        # merge in reverse order - while theres still elements in both
+        # merge in rev order, while there are elements in both arrays
         while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
+                # go to last pos and replace
                 nums1[last] = nums1[m - 1]
+                #update ptrs
                 m -= 1
             else:
-                nums1[last] = nums2[n - 1]
+                nums1[last] = nums2[n -1]
                 n -= 1
             last -= 1
-        
-        # fill nums1 with leftover nume2 elements
+        # edge case - if nums2 is not empty - fill nums1 w leftover nums2
         while n > 0:
             nums1[last] = nums2[n - 1]
-            n, last = n - 1, last - 1
-            
+            n, last = n - 1, last -1
+        
+        
+        
         
