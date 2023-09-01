e23e152453961102ae5ad8c60fd385e5e45699d6
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-01 17:34:54 -0500

    Time: 12 ms (96.32%), Space: 13.3 MB (25.74%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,12 +3,12 @@ class Solution(object):
         last = m + n - 1
         
         while m > 0 and n > 0:
-            if nums1[m-1] < nums2[n-1]:
-                nums1[last] = nums2[n-1]
-                n -= 1
+            if nums1[m -1] > nums2[n - 1]:
+                nums1[last] = nums1[m-1]
+                m -= 1
             else:
-                nums1[last] = nums1[m -1]
-                m -=1
+                nums1[last] = nums2[n - 1]
+                n -= 1
             last -= 1
         
         while n > 0:
