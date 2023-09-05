97da694f57b949a93e75e092d9d2435836320f92
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-04 20:55:41 -0500

    Time: 53 ms (85.88%), Space: 13.9 MB (38.82%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,21 +1,17 @@
 class Solution(object):
     def longestWord(self, words):
         words.sort()
-
-        # Create a set to store the words that can be built
+        
         built_words = set()
-
-        # Initialize the longest word found so far
-        longest = ""
-
+        longest_word = ""
+        
         for word in words:
-            # If the word has only one character or the prefix of the word is in built_words
             if len(word) == 1 or word[:-1] in built_words:
-                # Add the word to the built_words set
                 built_words.add(word)
-
-                # Update the longest word if needed
-                if len(word) > len(longest):
-                    longest = word
-
-        return longest
+            
+                # for last word if it's longest
+                if len(word) > len(longest_word):
+                    longest_word = word
+                
+        return longest_word
+                    
