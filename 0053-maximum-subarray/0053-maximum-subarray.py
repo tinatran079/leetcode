12936245d5ecb92087c398a65b33977fb6d0322e
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-05 14:28:49 -0500

    Time: 519 ms (86.20%), Space: 26.2 MB (53.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,22 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # nums - integer array
-        # find subarray with largest sum, return its sum
-        
         if len(nums) == 1:
-            return nums[0] # return single element itself
+            return nums[0]
         
-        max_sum = float('-inf')
-        current_total = 0
+        current_subarray = 0
+        max_subarray = float('-inf')
         
         for num in nums:
-            current_total += num
-            max_sum = max(max_sum, current_total)
+            current_subarray += num
+            max_subarray = max(current_subarray, max_subarray)
             
-            if current_total < 0:
-                current_total = 0
-            
-        return max_sum
-        
-        
+            if current_subarray < 0:
+                current_subarray = 0
+                
+        return max_subarray
