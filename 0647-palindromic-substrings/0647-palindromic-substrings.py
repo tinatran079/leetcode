b8963569c2a45a804a194e5944ae7e14f7a28d62
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-05 16:54:46 -0500

    Time: 69 ms (81.40%), Space: 13.2 MB (85.90%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,13 +1,17 @@
 class Solution(object):
     def expand(self, s, left, right):
         count = 0
+        
         while left >= 0 and right < len(s) and s[left] == s[right]:
             count += 1
             left -= 1
             right += 1
         return count
-    
     def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         total_count = 0
         
         for center in range(len(s)):
@@ -15,4 +19,4 @@ class Solution(object):
             total_count += self.expand(s, center, center + 1)
             
         return total_count
-
+        
