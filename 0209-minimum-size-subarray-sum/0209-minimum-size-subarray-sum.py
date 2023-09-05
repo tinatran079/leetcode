34368fc74ef9a94a763e9bf4e49286afb35edf07
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-05 16:08:14 -0500

    Time: 192 ms (66.11%), Space: 23.6 MB (77.31%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -5,17 +5,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left, total = 0, 0
+        # return the minimal LENGTH of a subarray whose sum
+        # is greater than or equal to target. 
+        l, total = 0, 0
         result = float('inf')
         
-        for right in range(len(nums)):
-            total += nums[right]
+        for r in range(len(nums)):
+            # calculate the current total
+            total += nums[r]
             while total >= target:
-                result = min(result, right - left + 1)
-                total -= nums[left]
-                left += 1
-                
+                result = min(result, r - l + 1)
+                total -= nums[l]
+                l += 1
+        
         return 0 if result == float('inf') else result
-            
+                
+                
             
         
