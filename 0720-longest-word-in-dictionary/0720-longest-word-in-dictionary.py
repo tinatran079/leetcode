3af8710335e8df35df3025bf4147528030a5d46b
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-04 21:01:19 -0500

    Time: 48 ms (95.29%), Space: 13.7 MB (97.65%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -2,15 +2,14 @@ class Solution(object):
     def longestWord(self, words):
         words.sort()
         
-        set_words = set()
+        built_words = set()
         longest = ""
         
         for word in words:
-            if len(word) == 1 or word[:-1] in set_words:
-                set_words.add(word)
+            if len(word) == 1 or word[:-1] in built_words:
+                built_words.add(word)
                 
                 if len(word) > len(longest):
                     longest = word
                     
         return longest
-    
