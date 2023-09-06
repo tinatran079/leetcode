036dd1379bc9e7e31ff360d0ea0632f9f48ef18d
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-06 11:32:10 -0500

    Time: 156 ms (61.60%), Space: 24.9 MB (20.12%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,12 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # mod k in case out of bounds
-        n = len(nums)
+        nums_length = len(nums)
         
-        k %= n
+        # mod k because it might be out of bounds
+        k %= nums_length
         
+        # reverse the entire array
         nums.reverse()
         
+        #grab the first part of the array up to k and rev
         nums[:k] = reversed(nums[:k])
+        
+        # grab the second part of the array k onward and rev
         nums[k:] = reversed(nums[k:])
+        
