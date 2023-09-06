0fa639e2a2ff40ddb4fb30dd3ca4818506a928fa
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-06 10:55:15 -0500

    Time: 35 ms (91.12%), Space: 14.2 MB (32.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,18 +1,14 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        # nums - array of integrs, target - integer
-        # return indices of 2 numbers that they add up to target
-        # [2, 7, 11, 15] , target = 9 -> [0,1] 
-                                        # 2 + 7 = 9
-        
-        previous = {} # num: index
         result = []
         
+        prev = {} # indx: num
+        
         for index, num in enumerate(nums):
-            difference = target - num
-            if difference in previous:
-                result.append(previous[difference])
+            diff = target - num
+            if diff in prev:
+                result.append(prev[diff])
                 result.append(index)
                 return result
-            previous[num] = index 
+            prev[num] = index
         
