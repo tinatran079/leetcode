d0092aac8ad55432e43ed02604fddcbbaa882690
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-07 13:47:34 -0500

    Time: 106 ms (54.28%), Space: 18.1 MB (73.08%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,19 +4,18 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        intervals.sort(key = lambda x: x[0])
-        
+        # need to sort the intervals first!!!
+        intervals.sort(key = lambda x: x[0]) # sort by the first val in intervals
         merged = [intervals[0]]
         
-        for i in range(1, len(intervals)):
+        for i in range(1, len(intervals)): # iterate through each interval in intervals
             current_interval = intervals[i]
-            last_merged_interval = merged[-1]
+            last_merged_interval = merged[-1] # grab the last interval in merged
             
-            if current_interval[0] <= last_merged_interval[1]:
-                last_merged_interval[1] = max(last_merged_interval[1], current_interval[1])
+            if last_merged_interval[1] >= current_interval[0]:
+                last_merged_interval[1] = (max(last_merged_interval[1], current_interval[1]))
                 
             else:
                 merged.append(current_interval)
-                
+            
         return merged
-        
