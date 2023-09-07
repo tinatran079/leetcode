c772229588af00dba751427de7c7bdd0002df62f
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-07 10:31:50 -0500

    Time: 1032 ms (69.87%), Space: 30.1 MB (25.16%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -4,15 +4,17 @@ class Solution(object):
         :type temperatures: List[int]
         :rtype: List[int]
         """
-        stack = []
-        res = [0] * len(temperatures)
+        stack = [] # temp, index
+        result = [0] * len(temperatures)
         
         
         for index, temp in enumerate(temperatures):
-            while stack and temp > stack[-1][0]: # temp > stack's current temp
-                stackT, stackI = stack.pop()
-                res[stackI] = index - stackI
+            while stack and temp > stack[-1][0]:
+                # pop
+                stackTemp, stackIndex = stack.pop()
+                # update result
+                result[stackIndex] = index - stackIndex
                 
             stack.append([temp, index])
             
-        return res
+        return result
