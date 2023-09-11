b2c6cd463e9e7da883240b2c75393a454dc9de6a
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-11 17:19:20 -0500

    Time: 281 ms (27.49%), Space: 14.6 MB (64.10%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,10 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        # make a new string of only alphanumeric char
+        # lowercase the new string
+        # have 2 ptrs at each end until it meets in middle
+        
         fixedS = ""
         
         for char in s:
-            if char.isalpha( ) or char.isdigit():
+            if char.isalpha() or char.isdigit():
                 fixedS += char
                 
         fixedS = fixedS.lower()
@@ -15,8 +19,8 @@ class Solution(object):
         l = 0
         r = len(fixedS) - 1
         
-        while l <= r: # go until the middle
-            if fixedS[l] != fixedS[r]: # not a palindrome
+        while l <= r:
+            if fixedS[l] != fixedS[r]:
                 return False
             l += 1
             r -= 1
