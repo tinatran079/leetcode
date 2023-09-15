61fb31772c16b8c8579df94ef98df42a9cba127b
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-15 10:49:11 -0500

    Time: 21 ms (28.34%), Space: 13.3 MB (35.50%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -4,21 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # return number of substrings of length 3 where there is no repeat
-        if len(s) < 3:
-            return 0
-        
         count = 0
         
-        for i in range(2, len(s)): # start at 0, 1, 2 (3 size)
-            # current substring window
-            current = s[i-2:i+1] #[0-2]
-            
-            # use a set
-            currentSet = set(current)
-            if len(currentSet) == 3:
+        for i in range(2, len(s)):
+            currentSubstring = s[i-2:i+1] # get three char
+            uniqueSet = set(currentSubstring)
+            if len(uniqueSet) == 3:
                 count += 1
-                
+        
         return count
-                
         
