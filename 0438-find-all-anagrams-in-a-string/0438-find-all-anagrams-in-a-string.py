cc6070efa66292aad55a4fd386598118b776753c
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-15 11:43:01 -0500

    Time: 71 ms (76.65%), Space: 14.4 MB (58.45%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -5,32 +5,32 @@ class Solution(object):
         :type p: str
         :rtype: List[int]
         """
-        # anagram - word of phrase formed by rearranging the letters of a diff
-        # word or phrase
-        
-        # return an array of all the start indices of p's anagrams in s
         if len(p) > len(s):
             return []
         
         pCount, sCount = {}, {}
+        res = []
         
         for i in range(len(p)):
             pCount[p[i]] = 1 + pCount.get(p[i], 0)
             sCount[s[i]] = 1 + sCount.get(s[i], 0)
-        
-        # compare first substring    
-        res = [0] if sCount == pCount else []
-        l = 0
-        for r in range(len(p), len(s)):
-            sCount[s[r]] = 1 + sCount.get(s[r], 0)
-            sCount[s[l]] -= 1
             
-            # if char is zero
-            if sCount[s[l]] == 0:
-                sCount.pop(s[l])
-            l+= 1
-            if sCount == pCount:
-                res.append(l)
-                
-        return res
+        res = [0] if sCount == pCount else []
+            
+        # check for rest of subarray
+        left = 0
         
+        for right in range(len(p), len(s)): # slide the window over
+            sCount[s[right]] = 1 + sCount.get(s[right], 0)
+            sCount[s[left]] -= 1
+            
+            if sCount[s[left]] == 0:
+                sCount.pop(s[left])
+            left += 1
+            if sCount == pCount:
+                res.append(left)
+            
+        return res
+            
+            
+            
