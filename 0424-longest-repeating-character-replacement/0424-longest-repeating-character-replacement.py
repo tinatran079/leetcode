d1ff9babc2dd46720109df081d40bb00d6f11e60
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-15 10:35:28 -0500

    Time: 101 ms (38.13%), Space: 13.6 MB (68.62%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,17 +5,23 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # return the longest substring containing the same letter
-        maxLen = 0
+        # return the length of the longest substring
+        # containing the same letter
+        # sliding window
         # hash map to keep track of all char
-        count = {}
+        # want to replace the char that occurs least frequently
+        res = 0
         l = 0
+        count = {}
         
         for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-            while (r-l+1) - max(count.values()) > k: # window not valid
+            count[s[r]] = 1+ count.get(s[r], 0) # add the cur element to the dict
+            # see if window is not valid
+            while (r - l + 1) - max(count.values()) > k: # if there aren't enough moves
                 count[s[l]] -= 1
-                l += 1
-            maxLen = max(maxLen, r - l +1)
-    
-        return maxLen
+                l += 1# increase window until find valid window
+            res = max(r-l+1 ,res)
+
+        
+        return res
+        
