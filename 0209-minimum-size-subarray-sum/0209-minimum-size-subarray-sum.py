1bb5e47ec19ab1a246a8dd03ccd42f9d0ccd5e40
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-15 10:59:32 -0500

    Time: 187 ms (72.38%), Space: 23.6 MB (78.34%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -5,18 +5,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # RETURN MIN LENGTH of SUBARRAY WHOSE
-        # SUM is >= TARGET, if none return 0
+        # return MINIMAL LENGTH OF SUBARRAY whose SUM is >= than target
+        # if there is no subarray return 0
+        # use sliding window algo
         
-        res = float('inf')
-        cur_sum = 0
-        i = 0
+        res = float('inf') # used for length of array
+        left = 0
+        curSum = 0 # used for sum of subarray
         
-        for j in range(len(nums)):
-            cur_sum += nums[j]
-            while cur_sum >= target:
-                cur_sum -= nums[i]
-                res = min(res, j - i + 1)
-                i += 1
-                
+        for right in range(len(nums)):
+            curSum += nums[right]
+            while curSum >= target: # found a valid window
+                curSum -= nums[left]
+                res = min(right - left + 1, res)
+                left += 1
+            
         return 0 if res == float('inf') else res
+                
