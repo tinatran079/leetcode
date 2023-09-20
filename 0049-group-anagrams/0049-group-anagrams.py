979881f2e5eab4c66050e838035a9adb458443c6
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-20 10:09:04 -0500

    Time: 66 ms (83.97%), Space: 16.9 MB (97.21%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagrams = {}
+        
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            
+            if sorted_s in anagrams:
+                anagrams[sorted_s].append(s)
+            else:
+                anagrams[sorted_s] = [s]
+                
+        return list(anagrams.values())
