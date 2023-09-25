db556739854db35e9895eb78799f2edf5871a515
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-25 10:45:29 -0500

    Time: 403 ms (59.39%), Space: 13.4 MB (80.83%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,29 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        # keep track of length of cur substring
-        # keep track of longest substring
-        
-        resLen = 0
+        # given string s, return longest palindromic substring ins
+        # need to account for odd and even length substrings
         res = ""
+        resLen = 0
         
         for i in range(len(s)):
-            # odd
             l, r = i, i
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r - l + 1) > resLen:
-                    resLen = (r - l + 1)
-                    res = s[l:r +1]
+                    res = s[l:r+1]
+                    resLen = r - l + 1 # update that substring length / window
                 l -= 1
                 r += 1
-
-            # even
+                
             l, r = i, i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r - l + 1) > resLen:
-                    resLen = (r - l + 1)
-                    res = s[l:r + 1]
+                    res = s[l:r+1]
+                    resLen = r - l + 1 # update that substring length / window
                 l -= 1
                 r += 1
                 
         return res
+        
