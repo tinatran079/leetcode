64a120cd53f65bcaa90e648017629fdaddfe6be3
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-09-25 10:57:05 -0500

    Time: 412 ms (55.61%), Space: 13.5 MB (54.34%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -11,18 +11,16 @@ class Solution(object):
             l, r = i, i
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r - l + 1) > resLen:
-                    res = s[l:r+1]
                     resLen = r - l + 1
+                    res = s[l:r + 1]
                 l -= 1
                 r += 1
             l, r = i, i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r - l + 1) > resLen:
-                    res = s[l:r+1]
                     resLen = r - l + 1
+                    res = s[l:r + 1]
                 l -= 1
                 r += 1
-                
         return res
-                    
         
