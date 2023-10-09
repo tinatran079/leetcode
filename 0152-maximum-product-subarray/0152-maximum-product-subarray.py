2d983d5ea23c459fedf2b8d5f3897ede4da02722
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-10-08 19:50:18 -0500

    Time: 57 ms (21.96%), Space: 13.7 MB (57.95%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -13,8 +13,8 @@ class Solution(object):
         for i in range(1, len(nums)):
             if nums[i] < 0:
                 numsMax, numsMin = numsMin, numsMax
-            numsMax = max(nums[i] * numsMax, max(nums[i] * numsMax, nums[i]))
-            numsMin = min(nums[i] * numsMin, min(nums[i] * numsMin, nums[i]))
+            numsMax = max(nums[i], numsMax * nums[i])
+            numsMin = min(nums[i], numsMin * nums[i])
             
             res = max(res, numsMax)
             
