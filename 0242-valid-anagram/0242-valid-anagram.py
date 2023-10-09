8a1c2e8173092a642460d7b73287dad31e73ebf5
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-10-08 21:37:23 -0500

    Time: 29 ms (79.49%), Space: 13.6 MB (82.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,13 +7,12 @@ class Solution(object):
         """
         return self.countChar(s) == self.countChar(t)
         
-        
-        
-    def countChar(self, anagram):
-        dict = {} # char: count
-        for char in anagram:
+    def countChar(self, anagrams):
+        dict = {}
+        for char in anagrams:
             if char not in dict:
                 dict[char] = 0
             dict[char] += 1
             
         return dict
+        
