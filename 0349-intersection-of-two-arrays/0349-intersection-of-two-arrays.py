4de3bce7bd04447aba1bad1853f6399bf4683118
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-10-08 21:11:36 -0500

    Time: 20 ms (93.73%), Space: 13.4 MB (67.07%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -5,21 +5,15 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        nums1.sort()
-        nums2.sort()
-        i = 0
-        j = 0
         res = []
+        items = set()
         
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] == nums2[j]:
-                res.append(nums1[i])
-                i += 1
-                j += 1
-            elif nums1[i] < nums2[j]: # nums[i] is smaller, so want to increment
-                i += 1
-            else:
-                j += 1
-
+        for item in nums1:
+            items.add(item)
+            
+        for num in nums2:
+            if num in items:
+                res.append(num)
+                
         return list(set(res))
         
