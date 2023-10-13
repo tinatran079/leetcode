a82908d906f482f80e18ee635963e51154c83f14
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-10-12 19:17:13 -0500

    Time: 771 ms (57.07%), Space: 22.4 MB (62.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,20 +4,20 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        # buy low sell high - buy before selling
-        buy = 0
-        sell = 1
+        #buy low, sell high
+        # profit if buy[i] < sell[i]
+        
         maxP = float('-inf')
         
+        buy = 0
+        sell = 1
+        
         while sell < len(prices):
-            # profit?
-            if prices[sell] > prices[buy]:
+            # profit
+            if prices[buy] < prices[sell]:
                 maxP = max(maxP, prices[sell] - prices[buy])
             else:
-                # buying price is greater than selling price
                 buy = sell
             sell += 1
-            
-        return 0 if maxP == float('-inf') else maxP
-                
         
+        return 0 if maxP == float('-inf') else maxP
