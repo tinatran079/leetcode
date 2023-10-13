a72b39743f87a7c360a22b4f7e68059c2924987e
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-10-13 12:20:12 -0500

    Time: 18 ms (33.15%), Space: 13.2 MB (88.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,11 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        
         stack = []
         
         for char in s:
-            if char == '(' or char == '[' or char == '{':
+            if char == '(' or char =='{' or char == '[':
                 stack.append(char)
             elif stack and char == ')' and stack[-1] == '(':
                 stack.pop()
@@ -18,7 +17,5 @@ class Solution(object):
                 stack.pop()
             else:
                 return False
-                
+            
         return len(stack) == 0
-        
-        
