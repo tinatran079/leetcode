302ee8920816cdd62a4ed8ed60d0212915aeb718
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-10-16 16:13:40 -0500

    Time: 27 ms, Memory: 13.6 MB - LeetHub

---
+++ valid-anagram/valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return self.countChar(s) == self.countChar(t)
+        
+        
+    def countChar(self, string):
+        dict = {}
+        for char in string:
+            if char not in dict:
+                dict[char] = 0
+            dict[char] += 1
+        return dict
