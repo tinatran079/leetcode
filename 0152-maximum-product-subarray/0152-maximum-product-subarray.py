a1ea2668f2c2d981c16726b80af1a73dfed69837
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-10-16 18:37:44 -0500

    Time: 48 ms (76.62%), Space: 14.1 MB (12.23%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # find subarray that has the largest product
+        res = nums[0]
         maxPrdt = nums[0]
         minPrdt = nums[0]
-        res = nums[0]
         
         for i in range(1, len(nums)):
             if nums[i] < 0:
                 maxPrdt, minPrdt = minPrdt, maxPrdt
-            
-            maxPrdt = max(nums[i], nums[i]*maxPrdt)
-            minPrdt = min(nums[i], nums[i]*minPrdt)
+                
+            maxPrdt = max(nums[i], maxPrdt*nums[i])
+            minPrdt = min(nums[i], minPrdt*nums[i])
             
             res = max(res, maxPrdt)
-            
         return res
