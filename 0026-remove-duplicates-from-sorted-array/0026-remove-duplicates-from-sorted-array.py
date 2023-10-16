e5df3ac8c9a5b072a01c3beb509568e31ac8859c
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-10-16 13:44:52 -0500

    Time: 55 ms (76.88%), Space: 14.8 MB (46.99%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -7,8 +7,8 @@ class Solution(object):
         left = 1
         
         for right in range(1, len(nums)):
-            if nums[right] != nums[right - 1]:
+            if nums[right] != nums[right-1]:
                 nums[left] = nums[right]
                 left += 1
-            
+                
         return left
