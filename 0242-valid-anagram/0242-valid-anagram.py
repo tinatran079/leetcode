3d0cce3c05f0ae7b0a93cc8882f756b9371dda60
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-10-16 18:22:58 -0500

    Time: 29 ms (79.36%), Space: 13.8 MB (70.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,18 +5,16 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        # word or phrase formed by rearranging the letters 
+        # if both strings have the same character count, is anagram
         return self.countChar(s) == self.countChar(t)
         
         
         
-        
-        
-    def countChar(self, string):
+    def countChar(self, characters):
         dict = {}
-        
-        for char in string:
+        for char in characters:
             if char not in dict:
                 dict[char] = 0
             dict[char] += 1
-        
         return dict
