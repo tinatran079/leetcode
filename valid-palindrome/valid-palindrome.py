31c64996aefbf572e74c7101ef3c648c9a928b90
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-10-16 16:17:10 -0500

    Time: 322 ms, Memory: 14.5 MB - LeetHub

---
+++ valid-palindrome/valid-palindrome.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        fixedS = ''
+        
+        for char in s:
+            if char.isalpha() or char.isnumeric():
+                fixedS += char
+                
+        fixedS = fixedS.lower()
+        
+        left = 0
+        right = len(fixedS) - 1
+        
+        while left <= right:
+            if fixedS[left] != fixedS[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
