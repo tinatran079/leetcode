36855c0d51650db446fefb9927e3570995a00ba9
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-10-16 19:16:01 -0500

    Time: 76 ms, Memory: 14.1 MB - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -5,14 +5,12 @@ class Solution(object):
         :rtype: int
         """
         dict = {}
-        
         for char in s:
             if char not in dict:
                 dict[char] = 0
             dict[char] += 1
-            
+        
         for i in range(len(s)):
             if dict[s[i]] == 1:
                 return i
-            
         return -1
