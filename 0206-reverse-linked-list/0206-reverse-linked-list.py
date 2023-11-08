82cea7f7529fdac6f4ff0986c8e739f4547424de
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2023-11-08 16:55:32 -0600

    Time: 25 ms (13.27%), Space: 15 MB (91.64%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -9,18 +9,15 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        #    1 -> 2 -> 3 -> 4 -> 5
-        #P    C   N
+        #   1 -> 2 -> 3 -> 4
+        #P  C.   N
         
         cur = head
         prev = None
         
         while cur is not None:
-            next = cur.next
-            cur.next = prev
-            prev = cur
-            cur = next
-            
+            nxt = cur.next # save the next node
+            cur.next = prev # reverse pointer
+            prev = cur # update prev ptr
+            cur = nxt # update cur ptr
         return prev
-            
-            
