c938496b3b731b64b8d700ced2e730b05a2c695f
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-01-19 14:03:46 -0600

    Time: 22 ms (99.87%), Space: 12.5 MB (100.00%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,22 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # naive approach
-        # input: array of integers and a target integer
-        # return: indices of 2 numbers that they add up to targt
         
-        # 2 loops. if nums[i] + nums[j] = target, return i & j
         res = []
+        prev = {} # num: index
         
-        for i in range(len(nums)):
-                            # avoid checking same pair
-                            # want unique pairs
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    res.append(i)
-                    res.append(j)
-                    return res
-                
-        #[2, 5, 5, 11]
-        # i =2, j = 5, 5, 11
-        # i = 5, j = 5, 11
+        for index, num in enumerate(nums):
+            complement = target - num
+            if complement in prev:
+                res.append(index)
+                res.append(prev[complement])
+                return res
+            prev[num] = index
