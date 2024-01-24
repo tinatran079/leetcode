5db75b52400960138e418947ad66d6895162fffc
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-01-24 13:43:13 -0600

    Time: 164 ms (71.77%), Space: 19 MB (95.83%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,16 +5,18 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(nums)
-        res = [1] * n
-        
+        answer = [1] * n
+        # left traversal - updating each element in answer array for prdt of everything to its left
         left_prdt = 1
+        
         for i in range(n):
-            res[i] *= left_prdt
+            answer[i] = left_prdt
             left_prdt *= nums[i]
             
         right_prdt = 1
         for i in range(n-1, -1, -1):
-            res[i] *= right_prdt
+            answer[i] *= right_prdt
             right_prdt *= nums[i]
-        return res
+            
+        return answer
         
