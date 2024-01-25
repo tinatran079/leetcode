294d911bea4e5cb354bb46f07e3b015b0639ff1f
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-01-25 12:56:01 -0600

    Time: 405 ms (99.65%), Space: 27.6 MB (71.55%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,13 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        counts= {}
-        for num in nums:
-            if num not in counts:
-                counts[num] = 0
-            counts[num] += 1
-
-        for count in counts.values():
-            if count > 1:
-                return True
-        return False
+        return len(set(nums)) != len(nums)
