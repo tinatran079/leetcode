293e3413153200cd551139ec6eb4912dbdb097e2
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-01-25 12:54:37 -0600

    Time: 476 ms (20.41%), Space: 25.9 MB (89.15%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,18 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        # return True if value appears twice in array
-        # return False if every element is distinct
-        
-#         counts= {}
-#         for num in nums:
-#             if num not in counts:
-#                 counts[num] = 0
-#             counts[num] += 1
+        counts= {}
+        for num in nums:
+            if num not in counts:
+                counts[num] = 0
+            counts[num] += 1
 
-#         for count in counts.values():
-#             if count > 1:
-#                 return True
-#         return False
-
-        return len(set(nums)) != len(nums)
+        for count in counts.values():
+            if count > 1:
+                return True
+        return False
