44d04a362c2f6d17ddefa8ac4f1d04ba46292fbf
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-01-31 11:47:56 -0600

    Time: 26 ms (40.51%), Space: 11.8 MB (95.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,29 +5,30 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        l, r = 0, len(nums) - 1
+        #initialize pointers
+        l, r = 0 , len(nums) - 1
         
         while l <= r:
             mid = (l+r) // 2
             if target == nums[mid]:
                 return mid
             
-            # left sorted portion
+            # in left sorted portion
             if nums[l] <= nums[mid]:
-                if target > nums[mid] or target < nums[l]:
+                if target  > nums[mid] or target < nums[l]:
                     # search right portion
                     l = mid + 1
                 else:
                     # search left portion
                     r = mid - 1
-            # right sorted portion
+                    
             else:
                 if target < nums[mid] or target > nums[r]:
                     # search left portion
-                    r = mid - 1
+                    r = mid -1
                 else:
                     # search right portion
                     l = mid + 1
-                    
-        return - 1
+            
+        return -1
         
