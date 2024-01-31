bf5fde57f824ffa3ab40f5b255f8a42e95249beb
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-01-31 16:08:15 -0600

    Time: 539 ms (42.50%), Space: 22.3 MB (82.16%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -5,16 +5,28 @@ class Solution(object):
         :rtype: int
         """
         
+#         Brute force
+#         res = 0
+        
+#         for l in range(len(height)):
+#             for r in range(l+1, len(height)):
+#                 area = (r-l) * min(height[l], height[r])
+#                 res = max(area, res)
+                
+#         return res
+        
         res = 0
         l, r = 0, len(height) - 1
         
         while l < r:
             area = (r-l) * min(height[l], height[r])
-            res = max(res, area)
+            res = max(area, res)
             
+            # update left and right ptrs, want to max heights
             if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
                 
         return res
+            
