e590ac4480aa4b6573f87860088691325b20f8d4
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-02-07 13:18:47 -0600

    Time: 79 ms (66.43%), Space: 12.9 MB (71.85%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -5,31 +5,32 @@ class Solution(object):
         :type p: str
         :rtype: List[int]
         """
+        # len of p is bigger than s string - not possible for there to be anangram
         if len(p) > len(s):
             return []
+            
+        pCount, sCount = {}, {}
         
-        sCount, pCount = {}, {}
-        res = []
-        
-        for i in range(len(p)): # get the first subarray
-            sCount[s[i]] = 1 + sCount.get(s[i], 0)
+        # add characters to hash map
+        for i in range(len(p)):
             pCount[p[i]] = 1 + pCount.get(p[i], 0)
+            sCount[s[i]] = 1 + sCount.get(s[i], 0)
             
+        # check if initialized is equal    
         res = [0] if sCount == pCount else []
-    
-        # rest of subarray
-        left = 0
         
-        for right in range(len(p), len(s)):
-            sCount[s[right]] = 1 + sCount.get(s[right], 0)
-            sCount[s[left]] -= 1
+        l = 0
+        for r in range(len(p), len(s)):
+            sCount[s[r]] = 1 + sCount.get(s[r], 0)
+            sCount[s[l]] -= 1
             
-            if sCount[s[left]] == 0:
-                sCount.pop(s[left])
-                
-            left += 1
+            if sCount[s[l]] == 0:
+                sCount.pop(s[l])
+            l += 1
+            
             if sCount == pCount:
-                res.append(left)
+                res.append(l)
                 
         return res
+            
         
