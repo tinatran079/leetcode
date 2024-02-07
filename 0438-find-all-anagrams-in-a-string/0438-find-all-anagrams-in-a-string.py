fdfab64b99bdf94a01a002abf942b42ce072a701
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-02-07 13:45:01 -0600

    Time: 68 ms (82.32%), Space: 12.7 MB (83.71%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -5,32 +5,37 @@ class Solution(object):
         :type p: str
         :rtype: List[int]
         """
-        # len of p is bigger than s string - not possible for there to be anangram
+        # check is len of p is bigger than len of s
         if len(p) > len(s):
             return []
-            
+        
         pCount, sCount = {}, {}
         
-        # add characters to hash map
+        # inialize hash maps for p & s
         for i in range(len(p)):
             pCount[p[i]] = 1 + pCount.get(p[i], 0)
             sCount[s[i]] = 1 + sCount.get(s[i], 0)
             
-        # check if initialized is equal    
-        res = [0] if sCount == pCount else []
+        # check if the initalized counts match
+        res = [0] if pCount == sCount else []
         
+        #intialize window
         l = 0
+        
         for r in range(len(p), len(s)):
+            # Include the character at position r in the window
             sCount[s[r]] = 1 + sCount.get(s[r], 0)
+            # Exclude the character at position l from the window
             sCount[s[l]] -= 1
             
+            # remove l if zero
             if sCount[s[l]] == 0:
                 sCount.pop(s[l])
+            # increase l
             l += 1
             
+            # compare hash maps again
             if sCount == pCount:
                 res.append(l)
-                
-        return res
             
-        
+        return res
