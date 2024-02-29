2e5f715b9e0534c52596a0f4bfeeb5057b702ca7
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-02-29 11:59:04 -0600

    Time: 17 ms (68.42%), Space: 13.6 MB (84.87%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -4,17 +4,28 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def reverseList(self, head, prev=None):
+    def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
+        #   1 -> 2 -> 3 -> 4 -> 5
+        #                       P C    N
         
-        if head is None:
-            return prev
+        # initialize prev node
+        # cur = head of linked list
+        # temp variable, next
+        # have cur.next point to prev
+        # update the pointers one to the right
+        # repeat until end of list and return prev
         
-        next = head.next
-        head.next = prev
+        prev = None
+        cur = head
         
-        return self.reverseList(next, head)
-        
+        while cur is not None:
+            next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
+            
+        return prev
