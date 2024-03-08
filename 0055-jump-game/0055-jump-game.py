3d3d38fab5c3161a75e492ebcbe52d34ac241670
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-03-08 12:53:17 -0600

    Time: 347 ms (85.12%), Space: 12.6 MB (83.55%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -4,13 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        # start at end
         goal = len(nums) - 1
         
         for i in range(len(nums) -1, -1, -1):
-            # go backwards
             if i + nums[i] >= goal:
-                # i is pos we are jumping to
                 goal = i
                 
         return True if goal == 0 else False
+        
