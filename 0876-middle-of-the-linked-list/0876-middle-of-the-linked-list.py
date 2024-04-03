2959e2003bdfeed59426a54ea9c9c2905f887279
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-04-03 14:15:26 -0500

    Time: 22 ms (6.66%), Space: 11.6 MB (89.99%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -9,11 +9,11 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        fast = head
         slow = head
+        fast = head
         
         while fast is not None and fast.next is not None:
             slow = slow.next
             fast = fast.next.next
-            
+        
         return slow
