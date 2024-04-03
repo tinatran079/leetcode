6342b5680d70cd00e6e2176551d7a568bc5cb7f8
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-04-03 14:22:46 -0500

    Time: 30 ms (80.66%), Space: 18.7 MB (72.21%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -10,9 +10,10 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        slow, fast = head, head
+        slow = head
+        fast = head
         
-        while fast and fast.next:
+        while fast is not None and fast.next is not None:
             slow = slow.next
             fast = fast.next.next
             if slow == fast:
