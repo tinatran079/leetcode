c7678183fbecbeb08b4991e90d252e73d8d184b0
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-04-11 12:38:33 -0500

    Time: 124 ms (89.65%), Space: 19.4 MB (52.78%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -8,7 +8,9 @@ class Solution(object):
         r = len(s) - 1
         
         while l <= r:
-            s[l], s[r] = s[r], s[l]
+            s[l],s[r] = s[r], s[l]
             l += 1
             r -= 1
+            
+        return s
         
