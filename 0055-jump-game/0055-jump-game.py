dc45e6d4e5bfd7fdff548cd43c72eec5c7e81d8f
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-04-12 11:01:28 -0500

    Time: 351 ms (75.62%), Space: 12.9 MB (25.50%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -6,9 +6,9 @@ class Solution(object):
         """
         goal = len(nums) - 1
         
-        for i in range(len(nums) -1, -1, -1):
+        # start at last index and work to beg
+        for i in range(len(nums)-1, -1, -1):
             if i + nums[i] >= goal:
                 goal = i
                 
         return True if goal == 0 else False
-        
