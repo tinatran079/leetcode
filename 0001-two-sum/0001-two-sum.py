df5aadda5fbb6e07f06abba3ff8ed905fda6a77f
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-09-11 10:16:50 -0500

    Time: 38 ms (75.62%), Space: 12.5 MB (44.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,6 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # given an array of integers, and integer target
+        # return indices of 2 #s that add up to target, in an list
+        # num1 + num2 = target
+        # complement = target - num
+        # have a hash map to keep track of num and index
+        # use enumerate to access index and number
+        # loop through nums
+        # calculate complment
+        # if complement is in hash map, then we know we found the 2 #'s
+        # append the index from hash map and current indx of num
+        # if not, add the cur num and index to hash map
         
         res = []
         prev = {}
@@ -14,6 +25,7 @@ class Solution(object):
             if complement in prev:
                 res.append(prev[complement])
                 res.append(index)
-                return res 
             prev[num] = index
+            
+        return res
             
