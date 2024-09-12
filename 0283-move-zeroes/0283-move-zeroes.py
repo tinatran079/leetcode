451f7cb8dc0b62d52e571dd5d0a8795dc230ee67
Tina Tran <98185192+tinatran079@users.noreply.github.com>
2024-09-12 15:32:17 -0500

    Time: 195 ms (21.07%), Space: 12.9 MB (64.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,14 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # left pointer to swap with nonzero
-        # right pointer iterates through list and finds nonzero
+        l = 0
+        for r in range(len(nums)):
+            # if 0 ignore, if nonzero, swap
+            if nums[r]:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
         
-        left, right = 0, 0
-        
-        while right < len(nums):
-            if nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-            right += 1
-        
+        return nums
